ec8cfe068834f1eb945359dae4a819a77e8cc055
shadiy <you@example.com>
2024-12-08 15:14:11 +0100

    initial functionalty

---
+++ main.js
@@ -0,0 +1,24 @@
+let current_clip_slug = "";
+let current_clip_slug_index = 0;
+let clips = "";
+
+document.addEventListener('DOMContentLoaded', (e) => {
+    fetch('clips.json').then((res) => {
+        if (!res.ok) {
+            throw new Error(`HTTP error! Status: ${res.status}`);
+        }
+        return res.json();
+    }).then((data) => {
+        clips = data['clips'];
+        current_clip_slug = clips[current_clip_slug_index];
+    }).catch((error) => {
+        console.error("Unable to fetch data:", error);
+    });
+});
+
+function nextClip() {
+    current_clip_slug_index += 1;
+    current_clip_slug = clips[current_clip_slug_index];
+
+    document.querySelector('.clip').setAttribute('src', "https://clips.twitch.tv/embed?clip=" + current_clip_slug + "&parent=shadiy.github.io");
+}
